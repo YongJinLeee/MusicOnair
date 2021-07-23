3858cf9baf5c5acb0bc9ceb9ab7c52cbabb65f9b
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-07-23 13:36:55 +0900

    Feat : Main Storyborad Collection View 배치 및 HomeViewController 작성
    - cell 180*240 , imageView 1:1(180px)
    - Auto Layout 적용

---
+++ MusicOnair/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MusicOnair
+//
+//  Created by LeeYongJin on 2021/07/22.
+//
+
+import UIKit
+
+class  HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
