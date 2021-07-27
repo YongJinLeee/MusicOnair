fb246f5b263ff851bde24107fef1ceaef199e6ec
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-07-28 03:39:11 +0900

    Feat : UICollectionViewCell protocol 관련 작성 및 MVVM 패턴화
    - extension으로 UICollectionViewCell protocol 추가
    - Cell 개수 임의 지정
    - Cell에 넣을 data를 처리할 class TrackCollectionViewCell 구현 및 프로토콜에 적용(Veiw)
    - 앨범 데이터 관련 구조체(TrackInfo, AlbumInfo) 작성 (Model / ViewModel)

--- MusicOnair/HomeViewController.swift
+++ MusicOnair/HomeViewController.swift
@@ -28,6 +28,7 @@ extension HomeViewController: UICollectionViewDataSource {
             return UICollectionViewCell()
         }
         
+        return cell
     }
     
     //헤더뷰 구성
@@ -47,5 +48,62 @@ extension HomeViewController: UICollectionViewDelegate {
 }
 
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    // cell size [20 - cell - 20 - cell - 20]
+}
+
+// MVVM : View
+class TrackCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var albumCover: UIImageView!
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var artistName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        albumCover.layer.cornerRadius = 4
+        artistName.textColor = UIColor.systemGray2
+    }
+    
+    func cellDataUpdate(CellInfo: AlbumInfo) {
+        
+        songTitle.text = CellInfo.title
+        artistName.text = CellInfo.artist
+        albumCover.image = CellInfo.trackThumbnail
+    }
+    
+}
+//MVVM : Model
+struct TrackInfo {
+    let title: String
+    let artist: String
+    let albumTitle: String
+    let albumCover: UIImage
+    
+    init(title: String, artist: String, albumTitle: String, albumCover: UIImage) {
+        self.title = title
+        self.artist = artist
+        self.albumTitle = albumTitle
+        self.albumCover = albumCover
+        
+    }
+}
+// MVVM : ViewModel; 데이터처리
+struct AlbumInfo {
+    let title: String
+    let tracks: [TrackInfo]
+    
+    // 정보가 없을 수도 있는 값들에 대한 옵셔널 처리
+    var artist: String? {
+        return tracks.first?.artist
+    }
+    var trackThumbnail: UIImage? {
+        return tracks.first?.albumCover
+    }
+    
+    init (title: String, tracks: [TrackInfo]) {
+        self.title = title
+        self.tracks = tracks
+    }
     
 }
