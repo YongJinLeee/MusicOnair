65a929bb92df0af55daa8cce4960557accdc0a8b
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-10 15:30:29 +0900

    Storyboard Object - class connect

--- MusicOnair/PlayerViewController.swift
+++ MusicOnair/PlayerViewController.swift
@@ -11,6 +11,16 @@ import Foundation
 
 class PlayerViewController: UIViewController {
 
+    @IBOutlet weak var artworkImg: UIImageView!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var trackArtist: UILabel!
+    
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var currentTime: UILabel!
+    @IBOutlet weak var totalTime: UILabel!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
