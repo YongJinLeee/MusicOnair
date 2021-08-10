cf972cf1bbf0d08f4b6a9c5ebd62784b857f22ec
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-10 18:12:00 +0900

    Feat : Player View 호출 함수 구현
    - Header Handler, Cell Delegate에 클릭시 Player 호출하는 함수 구현

--- MusicOnair/Home/HomeViewController.swift
+++ MusicOnair/Home/HomeViewController.swift
@@ -45,11 +45,18 @@ extension HomeViewController: UICollectionViewDataSource {
                 return UICollectionReusableView()
             }
             header.update(with: todaysTrack)
-            header.tapHandler = { todaysTrack -> Void in
-                //player 활성화
+            header.tapHandler = { todaysTrack in
+                let playerStoryboard = UIStoryboard.init(name: "Player", bundle: nil)
+                    guard let PlayerViewCtrl = playerStoryboard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController else { return }
+                
+                PlayerViewCtrl.simplePlayer.replaceCurrentItem(with: todaysTrack)
+                
+                self.present(PlayerViewCtrl, animated: true, completion: nil)
+                
                 //console test
                 print("---------> Today's track title: \(todaysTrack.convertToTrack()?.title) ")
             }
+            
             
             
             return header
@@ -60,8 +67,19 @@ extension HomeViewController: UICollectionViewDataSource {
 }
 
 // protocol이 너무 많거나 길어지면 extention 사용
+// HomeView에서 Cell 클릭시 player 띄우기
 extension HomeViewController: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //player 활성화 (해당 스토리보드 호출)
+        let playerStoryboard = UIStoryboard.init(name: "Player", bundle: nil)
+        // 특정 indentifier를 가진 ViewConroller의 인스턴스화
+        guard let PlayerViewCtrl = playerStoryboard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController else { return }
+        
+        let currentTrackItem = trackManage.tracks[indexPath.item]
+        PlayerViewCtrl.simplePlayer.replaceCurrentItem(with: currentTrackItem)
+        
+        present(PlayerViewCtrl, animated: true, completion: nil)
+    }
 }
 
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
