2effe7dc0b9d454fd3e3cf88c0f4668fd1f50e48
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-08-10 22:38:47 +0900

    Feat : Slider seeking 관련 함수 구현
    - Slider로 해당 트랙의 재생 위치 찾는 함수 구현 ( CMTime 타입인 Seek 함수 프로토콜로 slider가 멈춘 지점에 대한 시간정보를 받아 AVPlayer의 seek 프로퍼티 업데이트, 해당구간으로 업데이트 하여 재생)
    - seeking 여부에 따른 슬라이더 및 재생위치 업데이트 함수 구현 (isSeeking, updateTime 함수)

--- MusicOnair/PlayerViewController.swift
+++ MusicOnair/PlayerViewController.swift
@@ -22,14 +22,15 @@ class PlayerViewController: UIViewController {
     
     // stored property
     let simplePlayer = SimplePlayer.shared
-    var isSeeking: Bool = false
     
+    var isSeeking: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updatePlayBtnUI()
         playerDataUpdate()
+        updateCurrentTime(time: CMTime.zero)
     }
     
     func playerDataUpdate() {
@@ -71,11 +72,37 @@ class PlayerViewController: UIViewController {
     
     @IBAction func endDrag(_ sender: UISlider) {
         isSeeking = false
-        
+        // false 상태에서 탐색 완료 구간에 대한 데이터 넘어가 해당구간을 재생할 수 있도록 해야함(slider 위치도 함께 업데이트)
     }
-    
+    // 탐색 버튼 동작(시간정보 파싱)
     @IBAction func seek(_ sender: UISlider) {
+        guard let currentItem = simplePlayer.currentItem else { return }
+        let position = Double(sender.value)
+        let seconds = currentItem.duration.seconds * position
+        let time = CMTime(seconds: seconds, preferredTimescale: 100)
+        
+        simplePlayer.seek(to: time)
     }
     
+    func updateCurrentTime(time: CMTime) {
+        currentTime.text = SecondsToString(sec: simplePlayer.currentTime)
+        totalTime.text = SecondsToString(sec: simplePlayer.totalDuratiopnTime)
+        
+        if isSeeking == false {
+            // 노래를 들으면서 슬라이더 버튼으로 탐색하면 위치가 변할 때마다 Slider가 업데이트 되어서 노래가 끊기므로
+            // isSeeking이 아닌 상태에서만 currentTime 데이터가 업데이트 되도록 구현
+            timeSlider.value = Float(simplePlayer.currentTime / simplePlayer.totalDuratiopnTime)
+        }
+    }
+    // seeking slider에서 받은 CMTime 값 string으로 변환하기
+    func SecondsToString(sec: Double) -> String {
+        guard sec.isNaN == false else {
+            return "00:00"
+        }
+        let totalSeconds = Int(sec)
+        let min = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "&02d:%02d", min, seconds)
+    }
     
 }
